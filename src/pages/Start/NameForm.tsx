6367e8af1301f108e48150ed34bc97a523c049e1
kir-gek <kirokronos@gmail.com>
2025-06-08 12:47:16 +0300

    mini fix

--- src/pages/Start/NameForm.tsx
+++ src/pages/Start/NameForm.tsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { CountdownTimer } from "../../components/CountDownTimer";
-import { Start } from "../../components/Invation/Start";
 
 export function NameForm() {
   const [name, setName] = useState("");
